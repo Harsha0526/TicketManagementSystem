35389a1588d53eebae8f8e2e0fbd17bc9676cf68
Harsha0526 <hgattu@pdx.edu>
2024-06-15 18:27:12 -0700

    Removed redundant annotations

--- src/main/java/com/growfin/ticketingSystem/repositories/OrganizationRepository.java
+++ src/main/java/com/growfin/ticketingSystem/repositories/OrganizationRepository.java
@@ -4,14 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import com.growfin.ticketingSystem.models.Organization;
 
-
-@Repository("organizationRepository")
+@Repository
 public interface OrganizationRepository extends JpaRepository<Organization, String> {
-	
-	@Query(nativeQuery = true, value = "SELECT org.* FROM organizations org join organization_secret_mappings org_mapping on org.secret_mapping_id = org_mapping.id WHERE org_mapping.secret= (:secret)")
-	Organization findOrgBySecret(@Param("secret") String secret);
 
+    @Query(nativeQuery = true, value = "SELECT org.* FROM organizations org JOIN organization_secret_mappings org_mapping ON org.secret_mapping_id = org_mapping.id WHERE org_mapping.secret = (:secret)")
+    Organization findOrgBySecret(@Param("secret") String secret);
 }
--- src/main/java/com/growfin/ticketingSystem/repositories/TicketRepository.java
+++ src/main/java/com/growfin/ticketingSystem/repositories/TicketRepository.java
@@ -4,18 +4,15 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import com.growfin.ticketingSystem.models.Ticket;
 
-
-@Repository("ticketRepository")
+@Repository
 public interface TicketRepository extends JpaRepository<Ticket, String> {
-	
-	
-	@Query(nativeQuery = true, value = "SELECT tic.* FROM tickets tic JOIN administrators admin on tic.admin_id = admin.id "
-			+ "WHERE (added_by = (:customer) or (:customer) = '') "
-			+ "and (status = (:status) or (:status) = '' ) and (admin.email = (:assingedTo) or (:assingedTo) = '') ")
-	List<Ticket> getTicketByQueryParam(String assingedTo, String status, String customer);
 
+    @Query(nativeQuery = true, value = "SELECT tic.* FROM tickets tic JOIN administrators admin ON tic.admin_id = admin.id "
+            + "WHERE (tic.added_by = (:customer) OR (:customer) = '') "
+            + "AND (tic.status = (:status) OR (:status) = '') AND (admin.email = (:assignedTo) OR (:assignedTo) = '')")
+    List<Ticket> getTicketsByQueryParams(@Param("assignedTo") String assignedTo, @Param("status") String status, @Param("customer") String customer);
 }
